381969b6208b4728fd2b0aeb13336a51ddf4eb25
Denis <thesocot@mail.ru>
2023-11-05 22:28:08 +0300

    cosmetic

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
 
 const About = () => {
   return (
+    <>
     <h1>About</h1>
+    <h2>Simple shopping Cart</h2>
+    <h2>Tools: React, TypeScript, Bootstrap</h2>
+    <h2>GitHub Repo: https://github.com/Scoteezy/shopping-cart-ts </h2>
+    </>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
 const Home = () => {
   return (
+   <>
     <h1>Home</h1>
+    <h2>Go to Store section and test</h2>
+    <h2>Info about project at About section</h2>
+    </>
   )
 }
 
